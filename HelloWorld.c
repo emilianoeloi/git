5c5f1c1ec17316ce5d76fa062ca17600430e37db
Emiliano S. Barbosa <emilianoeloi@gmail.com>
2016-05-29 23:40:11 -0300

    ADD: implementing c helloWorld

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+/*
+ * http://www.programmingsimplified.com/c-hello-world-program
+ */
+#include <stdio.h>
+
+int main() {
+  char string[] = "Hello World";
+  printf("%s\n", string);
+  return 0;
+}
--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 java:
 	@javac HelloWorldApp.java
 	@java HelloWorldApp
+
+c:
+	@clang -o helloWorldApp HelloWorld.c
+	@./helloWorld
